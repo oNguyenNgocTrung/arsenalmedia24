c23a82be991cef7578e46a8e77f43d0bda2deb99
trungnn142 <trungnn142@gmail.com>
2016-10-23 00:50:40 +0700

    ref #N/A init project

---
+++ config/application.rb
@@ -0,0 +1,21 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module Arsenalmedia24
+  class Application < Rails::Application
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+    config.i18n.load_path += Dir["#{Rails.root.to_s}/config/locales/**/*.{rb,yml}"]
+    config.autoload_paths << Rails.root.join("lib")
+
+    Dir.glob("config/routes/*").each do |route|
+      config.paths["config/routes.rb"] << Rails.root.join(route)
+    end
+  end
+end
