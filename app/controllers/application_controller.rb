f5d474c6b63ad4b72591d636b8d7967b6de42023
trungnn142 <trungnn142@gmail.com>
2016-11-01 19:07:47 +0700

    ref task#N/A set multiple language

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  
+  before_action :set_locale
+
+  def set_locale
+    if cookies[:educator_locale] && I18n.available_locales.include?(
+      cookies[:educator_locale].to_sym)
+      l = cookies[:educator_locale].to_sym
+    else
+      l = I18n.default_locale
+      cookies.permanent[:educator_locale] = l
+    end
+    I18n.locale = l
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root "static_pages#home"
+  get "/change_locale/:locale", to: "settings#change_locale", as: :change_locale
 end
